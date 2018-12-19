e585a38c30acda4d5eef3f08b0e271c2f8378450
jollyzhe <jollyzhe123@gmail.com>
2018-12-19 16:26:05 +0800

    修改方法

--- FirstGit/src/com/xuanyi/git/fristGit.java
+++ FirstGit/src/com/xuanyi/git/fristGit.java
@@ -4,4 +4,12 @@ public class fristGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
     }
+
+    public void test(){
+        int a = 10 ;
+        System.out.println(a);
+    }
+
+
+
 }
