b1bf052cfa317bddfa079c681dfd8b939e6f927a
jollyzhe <jollyzhe123@gmail.com>
2018-12-19 16:35:06 +0800

    test

---
+++ git-test-project/src/com/xuanyi/git/FristGit.java
@@ -0,0 +1,12 @@
+package com.xuanyi.git;
+
+public class FristGit {
+
+    public static void main(String[] args) {
+        System.out.println("nihao,git");
+        System.out.println("nihao,git");
+        System.out.println("nihao,git");
+        System.out.println("nihao,git");
+        System.out.println("nihao,git");
+    }
+}
